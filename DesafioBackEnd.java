72224c822a76c1eeddf7cf6eb477858f63e66838
Paulo Andrade <pcdajr@gmail.com>
2019-01-06 14:28:57 -0300

    Criando e subindo arquivo em java
    Teste do processo seletivo - Estágio Senai

---
+++ DesafioBackEnd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DesafioBackEnd {
+
+    public static void main(String[] args) {
+        int resposta = 1;
+        System.out.println("Informe seu código:");
+        Scanner ler = new Scanner(System.in);
+        String entrada = ler.next();
+        int tamanho = entrada.length();
+        char[] lista_entrada = entrada.toCharArray();
+
+        for (int i = 0; i < (tamanho); i++) {
+            if (Integer.parseInt(String.valueOf(lista_entrada[i])) <= 2 && (Integer.parseInt(String.valueOf(lista_entrada[i + 1])) <= 6)) {
+                resposta += 1;
+            }
+        }
+
+         System.out.printf("Total de combinações: %d ",resposta);
+    }
+}
