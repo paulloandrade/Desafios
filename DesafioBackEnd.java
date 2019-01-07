ad1335932869fe48a095abc970a0887e3a27aa22
Paulo <pcdajr@gmail.com>
2019-01-07 02:19:42 -0300

    teste

--- DesafioBackEnd.java
+++ DesafioBackEnd.java
@@ -16,6 +16,6 @@ public class DesafioBackEnd {
             }
         }
 
-         System.out.printf("Total de combinações: %d ",resposta);
+         System.out.printf("Total de combinações teste: %d ",resposta);
     }
 }
